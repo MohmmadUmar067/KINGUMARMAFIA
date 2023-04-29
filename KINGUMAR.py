2e0e9fb5749a843e5ffabec6b5ba1f08417a0651
MohmmadUmar067 <130560004+MohmmadUmar067@users.noreply.github.com>
2023-04-29 20:54:04 +0500

    Delete KINGUMAR.py

--- KINGUMAR.py
+++
@@ -1,9 +0,0 @@
-import os,platform
-os.system('git pull')
- 
-MAFIA=platform.architecture()[0]
-if MAFIA=="32bit":
-    print('Sorry 32 Bit not Supported...')
-elif MAFIA=="64bit":
-    #print('Command is in update wait we will fix it soon !')
-    __import__("KINGUMAR")
