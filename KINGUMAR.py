bda47748cc5d54db142f30991a6b0ec9d82e2840
MohmmadUmar067 <130560004+MohmmadUmar067@users.noreply.github.com>
2023-04-29 20:55:47 +0500

    KINGUMAR.py
    git clone https://github.com/MohmmadUmar067/KINGUMARMAFIA
    cd KINGUMARMAFIA
    python KINGUMAR.py

---
+++ KINGUMAR.py
@@ -0,0 +1,9 @@
+import os,platform
+os.system('git pull')
+ 
+MAFIA=platform.architecture()[0]
+if KINGUMAR=="32bit":
+    print('Sorry 32 Bit not Supported...')
+elif KINGUMAR=="64bit":
+    #print('Command is in update wait we will fix it soon !')
+    __import__("KINGUMAR")
