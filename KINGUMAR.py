97efd0b49f07323e266e53fc6c7456b18aa14a7f
MohmmadUmar067 <130560004+MohmmadUmar067@users.noreply.github.com>
2023-04-29 20:01:46 +0500

    Update KINGUMAR.py

--- KINGUMAR.py
+++ KINGUMAR.py
@@ -2,8 +2,8 @@ import os,platform
 os.system('git pull')
  
 MAFIA=platform.architecture()[0]
-elif MAFIA=="32bit":
-    print('Sorry 32 Bit Supported...')
+if MAFIA=="32bit":
+    print('Sorry 32 Bit not Supported...')
 elif MAFIA=="64bit":
     #print('Command is in update wait we will fix it soon !')
     __import__("KINGUMAR")
